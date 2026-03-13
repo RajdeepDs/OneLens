449c276b1dddd0d84c11ade5cc16de779883ce89
Rajdeep Das <rajdeepds626@gmail.com>
2026-03-13 14:55:02 +0000

    refactor: cleanup

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,33 +1,36 @@
 "use client";
 import Link from "next/link";
-
 import { ModeToggle } from "./mode-toggle";
+import { WordMark } from "./ui/word-mark";
 import UserMenu from "./user-menu";
 
 export default function Header() {
-  const links = [
-    { to: "/", label: "Home" },
-    { to: "/dashboard", label: "Dashboard" },
-  ] as const;
+	const links = [
+		{ to: "/", label: "Changelog" },
+		{ to: "/dashboard", label: "Contact" },
+	] as const;
 
-  return (
-    <div>
-      <div className="flex flex-row items-center justify-between px-2 py-1">
-        <nav className="flex gap-4 text-lg">
-          {links.map(({ to, label }) => {
-            return (
-              <Link key={to} href={to}>
-                {label}
-              </Link>
-            );
-          })}
-        </nav>
-        <div className="flex items-center gap-2">
-          <ModeToggle />
-          <UserMenu />
-        </div>
-      </div>
-      <hr />
-    </div>
-  );
+	return (
+		<div>
+			<div className="container mx-auto flex flex-row items-center justify-between px-2 py-1">
+				<Link href={"/"}>
+					<WordMark />
+				</Link>
+				<nav className="flex gap-4 text-[13px]">
+					{links.map(({ to, label }) => {
+						return (
+							<Link href={to} key={to}>
+								{label}
+							</Link>
+						);
+					})}
+				</nav>
+				<div className="flex items-center gap-2">
+					<ModeToggle />
+					<UserMenu />
+				</div>
+			</div>
+			<hr />
+		</div>
+	);
 }
